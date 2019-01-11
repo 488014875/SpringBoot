fc6f031d471cd70ae77a42144414123fe4365c38
488014875 <488014875@qq.com>
2019-01-11 10:39:14 +0800

    网关，容错，负载均衡，注册中心

---
+++ rabbitmq/src/main/java/com/rabbitmq/RabbitMqApplication.java
@@ -0,0 +1,11 @@
+package com.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+
+public class RabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqApplication.class, args);
+    }
+
+}
